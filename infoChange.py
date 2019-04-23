72c0b2e7ae290224107a887c68b88a31ef14f258
INDEXxiaoxia <43598840+INDEXxiaoxia@users.noreply.github.com>
2019-04-23 17:52:41 +0800

    逻辑代码

--- infoChange.py
+++ infoChange.py
@@ -15,17 +15,28 @@ def infoC_Views(request):
 		else:
 			errMsg='successful!'
 	return render('11-infoC',local())
-
+{{ ThisUser.uname }}
+{{ ThisUser.pwd }}
+{{ ThisUser.Email }}
 def Card_Library_Views(request):
-
 	cardList=TaroLibrary.Object.filter()
 	cardList=[card for card in cardList]
+	# 整合url和对象
+	cardList=[list(card,ToIMG(card.id)) for card in cardList]
 	CardId=request.get['CardId']
 	ThisCard=TaroLibrary.Object.filter(CardId=CardId)
 	ThisImg='/static/img/taro'+str(CardId)+'.png'
 	return render('09-taroLibrary',local())
-
-
+{% for card in cardList %}
+{% if card.0.id>=11 %}
+	LargeAe
+{% else %}
+	LittleAe
+{% endif %}
+	{{ card.0.Cname }}
+	{{ Card.0.id }}
+	{{ Card.1 }}
+{% endfor %}
 
 
 
